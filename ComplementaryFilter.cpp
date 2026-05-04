b6144904b0974ed693dc5c80b06b033a44b703e5
teichen <paul.teichen@gmail.com>
2026-05-03 21:13:23 -0700

    complementary filter update

--- ComplementaryFilter.cpp
+++ ComplementaryFilter.cpp
@@ -63,7 +63,60 @@ void ComplementaryFilter::process(double dt, double* x, double* thrust, double* 
 
 void ComplementaryFilter::update(double* measurements)
 {
-    // TODO
+    utilities.set_elements(x_prior, x_post, n_s, 1);
+
+    // attitude
+    // high pass filter is input linear accelerometer and gyro magnetic data
+    // low pass filter is integrated angular velocity
+    double roll_from_acc = std::atan(measurements[model.mi_a_y] / measurements[model.mi_a_z]);
+    double pitch_from_acc = std::atan(-measurements[model.mi_a_x] / (measurements[model.mi_a_y] * std::sin(roll_from_acc) + measurements[model.mi_a_z] * std::cos(roll_from_acc)));
+    double yaw_from_acc = std::atan((measurements[model.mi_b_z] * std::sin(roll_from_acc) - measurements[model.mi_b_y] * std::cos(roll_from_acc)) / (measurements[model.mi_b_x] * std::cos(pitch_from_acc) + measurements[model.mi_b_y] * std::sin(pitch_from_acc) * std::sin(roll_from_acc) + measurements[model.mi_b_z] * std::sin(pitch_from_acc) * std::cos(roll_from_acc)));
+
+    if (!std::isnan(roll_from_acc))
+    {
+        x_post[model.si_theta_x] = alpha_att * x_prior[model.si_theta_x] + (1.0 - alpha_att) * roll_from_acc;
+    }
+    else
+    {
+        x_post[model.si_theta_x] = x_prior[model.si_theta_x];
+    }
+    if (!std::isnan(pitch_from_acc))
+    {
+        x_post[model.si_theta_y] = alpha_att * x_prior[model.si_theta_y] + (1.0 - alpha_att) * pitch_from_acc;
+    }
+    else
+    {
+        x_post[model.si_theta_y] = x_prior[model.si_theta_y];
+    }
+    if (!std::isnan(yaw_from_acc))
+    {
+        x_post[model.si_theta_z] = alpha_att * x_prior[model.si_theta_z] + (1.0 - alpha_att) * yaw_from_acc;
+    }
+    else
+    {
+        x_post[model.si_theta_z] = x_prior[model.si_theta_z];
+    }
+
+    // translation
+    // GPS velocity uses Doppler shifts and is more reliable than position
+    // high pass filter is input GPS velocity
+    // low pass filter is integrated linear accelerometer data
+    if (!std::isnan(measurements[model.mi_v_x]))
+    {
+        x_post[model.si_v_x] = alpha_trans * x_prior[model.si_v_x] + (1.0 - alpha_trans) * measurements[model.mi_v_x];
+    }
+    else
+    {
+        x_post[model.si_v_x] = x_prior[model.si_v_x];
+    }
+    if (!std::isnan(measurements[model.mi_v_y]))
+    {
+        x_post[model.si_v_y] = alpha_trans * x_prior[model.si_v_y] + (1.0 - alpha_trans) * measurements[model.mi_v_y];
+    }
+    else
+    {
+        x_post[model.si_v_y] = x_prior[model.si_v_y];
+    }
 }
 
 void ComplementaryFilter::initialize_state()
